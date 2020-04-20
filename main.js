a9d78a600b85c6fe350e5f835f5913a30ef83037
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-04-20 17:37:39 +0200

    push intermedia

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 var chilometri = prompt('Inserisca il numero di chilometri che intende percorrere.');
 console.log('Chilometri che intende percorrere:' + ' ' + chilometri + ' ' + 'km');
 
@@ -9,3 +10,20 @@ console.log('Prezzo per chilometro:' + ' ' + prezzo_per_chilometro);
 
 var prezzo_normale_biglietto = (chilometri * prezzo_per_chilometro);
 console.log('Prezzo biglietto:' + ' ' + prezzo_normale_biglietto);
+
+
+if (eta < 18) {
+
+    var prezzo_biglietto_minorenne = (prezzo_normale_biglietto - ((prezzo_normale_biglietto/100)*20));
+    console.log(prezzo_biglietto_minorenne);
+
+} else if (eta >= 65) {
+
+        var prezzo_biglietto_over = (prezzo_normale_biglietto - ((prezzo_normale_biglietto/100)*40));
+        console.log(prezzo_biglietto_over);
+
+} else {
+
+    console.log(prezzo_normale_biglietto);
+
+}
